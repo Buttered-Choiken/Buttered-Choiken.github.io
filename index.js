7a9f0bfcb4b757e85c8f11d0e321ef2a20bf0b07
Buttered-Choiken <82126165+Buttered-Choiken@users.noreply.github.com>
2021-04-07 21:57:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const UWU = document
+console.log('anytext')
+
+function butt() {
+UWU.getElementById('button').innerHTML = 'rfwefrfbqwwwrwraoijgrepojegraoij[aoriwegj[oijgaerj[oiergaoij[ergaijo[werfoij[fewoi[efwoi[efrwaoirag[OEFWIJRgoFGWOEIJ[ERAHOBI[efwpogjoiGRWJ[GWIORJJpefwiijOWOEGRI[HA'
+}
